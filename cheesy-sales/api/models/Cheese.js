0750281e03cb21bf516af895eaf4a75300fad431
Mohamed Ikhwan <1khwan.m0@gmail.com>
2018-04-20 20:35:05 +0530

    /CHZ-10_SALES-Add-Models--PARTIAL_COMMIT:
    -partial because we need to get cheese moving

---
+++ cheesy-sales/api/models/Cheese.js
@@ -0,0 +1,44 @@
+/**
+ * User.js
+ *
+ * A user who can log in to this application.
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    cheeseName: {
+      type: 'string',
+      required: true,
+      description: 'Full representation of the Cheese name',
+      example: 'Tiddy Cheese'
+    },
+
+    cheeseDescription: {
+      type: 'string',
+      required: true,
+      description: 'The cheese made from Tiddies'
+    },
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+    // n/a
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    // n/a
+
+
+  },
+
+
+};
