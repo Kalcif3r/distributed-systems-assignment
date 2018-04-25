f67ce9d156bcd96e1375939afeb31957983ffd1f
Senal Wijeratne <senal.ra128@gmail.com>
2018-04-25 12:31:44 +0530

    create/update policy added for inventory

--- cheesy-sales/config/policies.js
+++ cheesy-sales/config/policies.js
@@ -12,11 +12,15 @@ module.exports.policies = {
 
   '*': 'is-logged-in',
 
-  //Block create and update actions if the user is not a Manager or an Admin
+  //Block create and update actions for cheese & factory if the user is not a Manager or an Admin
   'Cheese/add-action' : ['is-manager', 'is-super-admin'],
   'Cheese/update-action' : ['is-manager', 'is-super-admin'],
   'Factory/add-action' : ['is-manager', 'is-super-admin'],
   'Factory/update-action' : ['is-manager', 'is-super-admin'],
+
+  //Block create and update actions if the user is a sales person
+  'Inventory/add-action' : ['is-manager', 'is-super-admin', 'is-stock-person'],
+  'Inventory/update-action' : ['is-manager', 'is-super-admin', 'is-stock-person'],
 
   //Block request for deleting if the user is not a Manager or an Admin
   'Inventory/archive-action' : ['is-manager', 'is-super-admin'],
