74291a26558dcc81c269ff05d40295f4f3b875b6
Senal Wijeratne <senal.ra128@gmail.com>
2018-04-25 17:18:00 +0530

    Policies fixed

--- cheesy-sales/config/policies.js
+++ cheesy-sales/config/policies.js
@@ -13,20 +13,20 @@ module.exports.policies = {
   '*': 'is-logged-in',
 
   //Block create and update actions for cheese & factory if the user is not a Manager or an Admin
-  'Cheese/add-action' : ['is-manager', 'is-super-admin'],
-  'Cheese/update-action' : ['is-manager', 'is-super-admin'],
-  'Factory/add-action' : ['is-manager', 'is-super-admin'],
-  'Factory/update-action' : ['is-manager', 'is-super-admin'],
+  'Cheese/add-action'         : 'is-admin-or-manager',
+  'Cheese/update-action'      : 'is-admin-or-manager',
+  'Factory/add-action'        : 'is-admin-or-manager',
+  'Factory/update-action'     : 'is-admin-or-manager',
 
   //Block create and update actions if the user is a sales person
-  'Inventory/add-action' : ['is-manager', 'is-super-admin', 'is-stock-person'],
-  'Inventory/update-action' : ['is-manager', 'is-super-admin', 'is-stock-person'],
+  'Inventory/add-action'      : 'is-admin-or-manager-or-stock-person',
+  'Inventory/update-action'   : 'is-admin-or-manager-or-stock-person',
 
   //Block request for deleting if the user is not a Manager or an Admin
-  'Inventory/archive-action' : ['is-manager', 'is-super-admin'],
-  'Factory/archive-action' : ['is-manager', 'is-super-admin'],
-  'Cheese/archive-action' : ['is-manager', 'is-super-admin'],
-  'Invoice/archive-action' : ['is-manager', 'is-super-admin'],
+  'Inventory/archive-action'  : 'is-admin-or-manager',
+  'Factory/archive-action'    : 'is-admin-or-manager',
+  'Cheese/archive-action'     : 'is-admin-or-manager',
+  'Invoice/archive-action'    : 'is-admin-or-manager',
 
   // Bypass the `is-logged-in` policy for:
   'entrance/*': true,
